d9d996eea8453303688d1a443d36375db48e2bcf
Yazzz28 <yacinemennaa@gmail.com>
2023-12-19 15:50:06 +0100

    quete - 09

---
+++ src/DataFixtures/ProgramFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Program;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ProgramFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $program = new Program();
+        $program->setTitle('Walking dead');
+        $program->setSynopsis('Des zombies envahissent la terre');
+        $program->setPoster('https://fr.web.img6.acsta.net/pictures/19/09/23/12/05/4768866.jpg');
+        $program->setCategory($this->getReference('category_Action'));
+        $manager->persist($program);
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+          CategoryFixtures::class,
+        ];
+    }
+
+
+}
+
