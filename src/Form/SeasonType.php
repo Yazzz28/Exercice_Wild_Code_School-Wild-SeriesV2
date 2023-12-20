bac72802b9c09a3a61e26b4111392996e171af3a
Yazzz28 <yacinemennaa@gmail.com>
2023-12-20 07:31:31 +0100

    quete - 15

---
+++ src/Form/SeasonType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Program;
+use App\Entity\Season;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SeasonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('number')
+            ->add('year')
+            ->add('description')
+            ->add('Program', null, [
+                'choice_label' => 'title',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Season::class,
+        ]);
+    }
+}
