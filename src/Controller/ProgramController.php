3c4489e215b2fc09321baf323b99a1c6e2589720
Yazzz28 <yacinemennaa@gmail.com>
2023-12-20 11:38:35 +0100

    quete - 21

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -7,9 +7,11 @@ use App\Entity\Episode;
 use App\Entity\Program;
 use App\Form\ProgramType;
 use App\Service\ProgramDuration;
+use Symfony\Component\Mime\Email;
 use App\Repository\ProgramRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
@@ -31,7 +33,7 @@ class ProgramController extends AbstractController
     }
 
     #[Route('/new', name: 'new')]
-    public function new(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    public function new(Request $request, MailerInterface $mailer,  EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $program = new Program();
         $form = $this->createForm(ProgramType::class, $program);
@@ -43,6 +45,14 @@ class ProgramController extends AbstractController
             $entityManager->persist($program);
             $entityManager->flush();
             $this->addFlash('success', 'The new program has been created');
+
+            $email = (new Email())
+                ->from($this->getParameter('mailer_from'))
+                ->to('your_email@example.com')
+                ->subject('Une nouvelle série vient d\'être publiée !')
+                ->html($this->renderView('Program/newProgramEmail.html.twig', ['program' => $program]));
+
+            $mailer->send($email);
 
             // Redirect to categories list
             return $this->redirectToRoute('program_index');
