f1f95e09736f35b709e7546b5fedd99d34075bb1
Yazzz28 <yacinemennaa@gmail.com>
2023-12-20 08:10:01 +0100

    quete - 17

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,12 +6,12 @@ use App\Entity\Season;
 use App\Entity\Episode;
 use App\Entity\Program;
 use App\Form\ProgramType;
-use App\Repository\SeasonRepository;
 use App\Repository\ProgramRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/program', name: 'program_')]
@@ -75,5 +75,37 @@ class ProgramController extends AbstractController
             'seasons' => $season,
             'episodes' => $episode,
         ]);
+    }
+
+    #[Route('/{id}/edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'], name: 'edit')]
+    public function edit(Request $request, Program $program, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ProgramType::class, $program);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();           
+            $this->addFlash('success', 'The program has been updated'); 
+
+            // Redirect to categories list
+            return $this->redirectToRoute('program_index');
+        }
+
+        return $this->render('program/edit.html.twig', [
+            'form' => $form,
+            'program' => $program,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
+    public function delete(Request $request, Program $program, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$program->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($program);
+            $entityManager->flush();
+            $this->addFlash('danger', 'The program has been deleted');
+        }
+
+        return $this->redirectToRoute('program_index', [], Response::HTTP_SEE_OTHER);
     }
 }
